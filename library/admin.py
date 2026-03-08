7857800d21b642b9e8092d9a7ebba9a0a42dfb06
ganiyu lukman <lganiyu43@gmail.com>
2026-03-08 19:54:05 +0100

    Register Book and Transaction models in admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Book, Transaction
+
+# Register your models here
+admin.site.register(Book)
+admin.site.register(Transaction)
